b47563133a55c32271d0a26592b5bc6f25811c32
vefs <weichen.vefs@gmail.com>
2016-01-01 15:26:29 +0800

    extend divide unit test

--- web-api/weichen.wu/test_api.py
+++ web-api/weichen.wu/test_api.py
@@ -44,14 +44,34 @@ class FlaskrTestCase(unittest.TestCase):
 #-------------------------------#
 #-------- multiply unit test
     def test_multiply_value(self):
-        rv = self.app.get('/multiply?value1=50&value2=1')
-        assert '50.00' in rv.data    
+        rv = self.app.get('/multiply?value1=2.33&value2=1.65')
+        assert '3.84' in rv.data    
         rv = self.app.get('/multiply?value1=1.33&value2=30')
         assert '39.9' in rv.data
         rv = self.app.get('/multiply?value1=2.5&value2=1.22')
-        assert '3' in rv.data
+        assert '3.0' in rv.data
+    def test_multiply_miss(self):
+        rv = self.app.get('/multiply?value1=1')
+        assert '406 Error' in rv.data
+    def test_multiply_invalid(self):
+        rv = self.app.get('/multiply?value1=a&value=2')
+        assert '406 Error' in rv.data
 #-------------------------------#
-#-------- dividex unit test
+#-------- divide unit test
+    def test_divide_value(self):
+        rv = self.app.get('/divide?value1=2.33&value2=1.65')
+        assert '1.41' in rv.data    
+        rv = self.app.get('/divide?value1=1.33&value2=30')
+        assert '0.04' in rv.data
+        rv = self.app.get('/divide?value1=2.5&value2=1.22')
+        assert '2.05' in rv.data
+    def test_divide_miss(self):
+        rv = self.app.get('/divide?value1=1')
+        assert '406 Error' in rv.data
+    def test_divide_invalid(self):
+        rv = self.app.get('/divide?value1=a&value=2')
+        assert '406 Error' in rv.data
+
 
 if __name__ == '__main__':
     unittest.main()
