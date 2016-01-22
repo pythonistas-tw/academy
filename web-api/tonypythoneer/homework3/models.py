8eedbc024a9859cd66dd6fc805d1a1ac3ea6237b
TonyPythoneer <tonypythoneer@gmail.com>
2016-01-22 16:35:16 +0800

    Enhance: Add new field in user and modify methods

--- web-api/tonypythoneer/homework3/models.py
+++ web-api/tonypythoneer/homework3/models.py
@@ -6,47 +6,57 @@
 """
 import datetime
 import hashlib
-import re
+# import re
 
 from sqlalchemy import Column, DateTime, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import validates
+# from sqlalchemy.orm import validates
 
 
 Base = declarative_base()
 
 
+def make_default_nickname(account):
+    """Fetch name of account as nickname"""
+    default_nickname, _ = account.split('@')
+    return default_nickname
+
+
+def make_password(password):
+    """Using sha1 to hash password"""
+    hashed_password = hashlib.sha1(password).hexdigest()
+    return hashed_password
+
+
 class User(Base):
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
+
+    # profile fields
     account = Column(String(255), unique=True, index=True)
     password = Column(String(128))
+    nickname = Column(String(255))
+
+    # timestamp fields
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     updated_at = Column(DateTime, onupdate=datetime.datetime.utcnow,
                         default=datetime.datetime.utcnow)
 
-    def __init__(self, account, password):
+    def __init__(self, account, password, nickname=None):
         self.account = account
         self.password = self._make_password(password)
+        self.nickname = nickname if not nickname else make_default_nickname(account)
 
     def __repr__(self):
         return "<User(account='%s')>" % (self.account)
 
-    '''
-    @validates('account')
-    def validate_account(self, fieldname, value):
-        assert bool(re.match(r"[^@]+@[^@]+\.[^@]+", value))
-        return value
-    '''
-
-    def _make_password(self, password):
-        hashed_password = hashlib.sha1(password).hexdigest()
-        return hashed_password
-
     def set_password(self, password):
-        self.password = self._make_password(password=password)
+        """Using hash lib to hash password"""
+        self.password = make_password(password=password)
 
     def check_password(self, password):
-        hashed_password = self._make_password(password=password)
+        """check password"""
+        hashed_password = make_password(password=password)
         is_correct = self.password is hashed_password
         return is_correct
