185ffba21c20c42469a1d006794261f0912ff431
Chia-Lin Cho <fox91119@gmail.com>
2016-02-03 13:57:44 +0800

    API-Exercise II

--- web-api/maomao/api.py
+++ web-api/maomao/api.py
@@ -1,50 +1,59 @@
 from flask import Flask
 from flask import request
+
 app = Flask(__name__)
 
 @app.route("/sum", methods=["GET"])
 def f_sum():
     if request.method == "GET":
         try:
-            value1 = float(request.args.get("value1", "0"))
-            value2 = float(request.args.get("value2", "0"))
-            result = value1+value2
+            value1 = request.args.get("value1")
+            value2 = request.args.get("value2")
+            if value1 == None or value2 == None:
+                return "406 Error"
+            result = float(value1)+float(value2)
             return str(result)
-        except Exception as e:
-            return "Error: {}".format(str(e))
+        except:
+            return "406 Error"
 
 @app.route("/minus", methods=["GET"])
 def f_minus():
     if request.method == "GET":
         try:
-            value1 = float(request.args.get("value1", "0"))
-            value2 = float(request.args.get("value2", "0"))
-            result = value1-value2
+            value1 = request.args.get("value1")
+            value2 = request.args.get("value2")
+            if value1 == None or value2 == None:
+                return "406 Error"
+            result = float(value1)-float(value2)
             return str(result)
-        except Exception as e:
-            return "Error: {}".format(str(e))
+        except:
+            return "406 Error"
 
 @app.route("/multiply", methods=["GET"])
 def f_multiply():
     if request.method == "GET":
         try:
-            value1 = float(request.args.get("value1", "0"))
-            value2 = float(request.args.get("value2", "1"))
-            result = str(value1*value2)
-            return result
-        except Exception as e:
-            return "Error: {}".format(str(e))
+            value1 = request.args.get("value1")
+            value2 = request.args.get("value2")
+            if value1 == None or value2 == None:
+                return "406 Error"
+            result = float(value1)*float(value2)
+            return str(result)
+        except:
+            return "406 Error"
 
 @app.route("/divide", methods=["GET"])
 def f_divide():
     if request.method == "GET":
         try:
-            value1 = float(request.args.get("value1", "0"))
-            value2 = float(request.args.get("value2", "1"))
-            result = str(value1/value2)
-            return result
-        except Exception as e:
-            return "Error: {}".format(str(e))
+            value1 = request.args.get("value1")
+            value2 = request.args.get("value2")
+            if value1 == None or value2 == None:
+                return "406 Error"
+            result = float(value1)/float(value2)
+            return str(result)
+        except:
+            return "406 Error"
 
 if __name__ == '__main__':
     app.run()
---
+++ web-api/maomao/flask_test.py
@@ -0,0 +1,153 @@
+import os
+import api
+import unittest
+import tempfile
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        api.app.config['TESTING'] = True
+        self.app = api.app.test_client()
+
+    def test_sum(self):
+    	# test correct
+        rv = self.app.get('/sum?value1=1&value2=1')
+        assert rv.data == b'2.0'
+
+        rv = self.app.get('/sum?value1=52&value2=7')
+        assert rv.data == b'59.0'
+
+        rv = self.app.get('/sum?value1=100.5&value2=33.1')
+        assert rv.data == b'133.6'
+
+        # test missing
+        rv = self.app.get('/sum?value1=1')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/sum?value2=10')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/sum')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/sum?value1=&value2=8')
+        assert rv.data == b'406 Error'
+
+        # test invalid
+        rv = self.app.get('/sum?value1=kk&value2=1')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/sum?value1=9&value2=ss1')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/minus?value1=p9&value2=121')
+        assert rv.data == b'406 Error'
+
+    def test_minus(self):
+    	# test correct
+        rv = self.app.get('/minus?value1=1&value2=1')
+        assert rv.data == b'0.0'
+
+        rv = self.app.get('/minus?value1=52&value2=7')
+        assert rv.data == b'45.0'
+
+        rv = self.app.get('/minus?value1=100.5&value2=33.1')
+        assert rv.data == b'67.4'
+
+        # test missing
+        rv = self.app.get('/minus?value1=1')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/minus?value2=10')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/minus')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/minus?value1=&value2=8')
+        assert rv.data == b'406 Error'
+
+        # test invalid
+        rv = self.app.get('/minus?value1=kk&value2=1')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/minus?value1=9&value2=ss1')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/minus?value1=p9&value2=121')
+        assert rv.data == b'406 Error'
+
+    def test_multiply(self):
+    	# test correct
+        rv = self.app.get('/multiply?value1=1&value2=1')
+        assert rv.data == b'1.0'
+
+        rv = self.app.get('/multiply?value1=52&value2=7')
+        assert rv.data == b'364.0'
+
+        rv = self.app.get('/multiply?value1=100.5&value2=33.1')
+        assert rv.data == b'3326.55'
+
+        # test missing
+        rv = self.app.get('/multiply?value1=1')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/multiply?value2=10')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/multiply')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/multiply?value1=&value2=8')
+        assert rv.data == b'406 Error'
+
+        # test invalid
+        rv = self.app.get('/multiply?value1=kk&value2=1')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/multiply?value1=9&value2=ss1')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/multiply?value1=p9&value2=121')
+        assert rv.data == b'406 Error'
+
+    def test_divide(self):
+        # test correct
+        rv = self.app.get('/divide?value1=1&value2=1')
+        assert rv.data == b'1.0'
+
+        rv = self.app.get('/divide?value1=10&value2=4')
+        assert rv.data == b'2.5'
+
+        rv = self.app.get('/divide?value1=52&value2=7')
+        assert rv.data == b'7.428571428571429'
+
+        rv = self.app.get('/divide?value1=100.5&value2=33.1')
+        assert rv.data == b'3.036253776435045'
+
+        # test missing
+        rv = self.app.get('/divide?value1=1')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/divide?value2=10')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/divide')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/divide?value1=&value2=8')
+        assert rv.data == b'406 Error'
+
+        # test invalid
+        rv = self.app.get('/divide?value1=kk&value2=1')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/divide?value1=9&value2=ss1')
+        assert rv.data == b'406 Error'
+
+        rv = self.app.get('/divide?value1=p9&value2=121')
+        assert rv.data == b'406 Error'
+
+
+if __name__ == '__main__':
+    unittest.main()
