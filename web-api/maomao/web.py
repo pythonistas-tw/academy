fc1c30d640dcc4734f90e8ba373818a996d01371
Chia-Lin Cho <fox91119@gmail.com>
2016-02-11 16:18:20 +0800

    Remove duplicate lines

--- web-api/maomao/web.py
+++ web-api/maomao/web.py
@@ -5,50 +5,31 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.errorhandler(406)
-def f_sum(value1, value2):
-    try:
-        result = float(value1)+float(value2)
-        msg = "The Answer of {} + {} is {}".format(value1, value2, result)
-        return render_template('count.html', msg=msg)
-    except ValueError:
-        return render_template('count.html', error="Invalid values"), 406
-    except:
-        return render_template('count.html', error="Unexpected error"), 406
+def f_sum(value1, value2, v1, v2):
+    result = v1+v2
+    msg = "The Answer of {} + {} is {}".format(value1, value2, result)
+    return render_template('count.html', msg=msg)
 
 @app.errorhandler(406)
-def f_minus(value1, value2):
-    try:
-        result = float(value1)-float(value2)
-        msg = "The Answer of {} - {} is {}".format(value1, value2, result)
-        return render_template('count.html', msg=msg)
-    except ValueError:
-        return render_template('count.html', error="Invalid values"), 406
-    except:
-        return render_template('count.html', error="Unexpected error"), 406
+def f_minus(value1, value2, v1, v2):
+    result = v1-v2
+    msg = "The Answer of {} - {} is {}".format(value1, value2, result)
+    return render_template('count.html', msg=msg)
 
 @app.errorhandler(406)
-def f_multiply(value1, value2):
-    try:
-        result = float(value1)*float(value2)
-        msg = "The Answer of {} * {} is {}".format(value1, value2, result)
-        return render_template('count.html', msg=msg)
-    except ValueError:
-        return render_template('count.html', error="Invalid values"), 406
-    except:
-        return render_template('count.html', error="Unexpected error"), 406
+def f_multiply(value1, value2, v1, v2):
+    result = v1*v2
+    msg = "The Answer of {} * {} is {}".format(value1, value2, result)
+    return render_template('count.html', msg=msg)
 
 @app.errorhandler(406)
-def f_divide(value1, value2):
+def f_divide(value1, value2, v1, v2):
     try:
-        result = float(value1)/float(value2)
+        result = v1/v2
         msg = "The Answer of {} / {} is {}".format(value1, value2, result)
         return render_template('count.html', msg=msg)
-    except ValueError:
-        return render_template('count.html', error="Invalid values"), 406
     except ZeroDivisionError:
         return render_template('count.html', error="Zero Division Error"), 406
-    except:
-        return render_template('count.html', error="Unexpected error"), 406
 
 operation = {"sum":f_sum, "minus":f_minus, "multiply":f_multiply, "divide":f_divide}
 
@@ -61,7 +42,11 @@ def f_count():
             value2 = request.args.get("value2")
             if op == None or op == "" or value1 == None or value1 == "" or value2 == None or value2 == "":
                 return render_template('count.html', error="Missing values"), 406
-            return operation[op](value1, value2)
+            v1 = float(value1)
+            v2 = float(value2)
+            return operation[op](value1, value2, v1, v2)
+        except ValueError:
+            return render_template('count.html', error="Invalid values"), 406
         except:
             return render_template('count.html', error="Unexpected error"), 406
 
