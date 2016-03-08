9f90b303215534f1b4b128ff06cc91d2678a1809
whale176 <seraphim.176@gmail.com>
2016-03-09 01:35:40 +0800

    [unfinished] finished successful test cases; failed test cases is still fixing

---
+++ web-api/whale176/webAPI/v1/calculate_api_test.py
@@ -0,0 +1,48 @@
+from calculate_api import app
+import json
+import os
+import unittest
+
+__author__ = 'whale176'
+
+class CalculateAPITest(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def test_sum_success(self):
+        tester = app.test_client(self)
+        response = tester.get('/sum?value1=1&value2=1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data), 2)
+
+    def test_minus_success(self):
+        tester = app.test_client(self)
+        response = tester.get('/minus?value1=1&value2=1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data), 0)
+
+    def test_multiply_success(self):
+        tester = app.test_client(self)
+        response = tester.get('/multiply?value1=1&value2=1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data), 1)
+
+    def test_divide_success(self):
+        tester = app.test_client(self)
+        response = tester.get('/divide?value1=1&value2=1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data), 1)
+
+    def test_divide_fail_with_invalidate_denominator(self):
+        tester = app.test_client(self)
+        response = tester.get('/divide?value1=1&value2=0')
+        self.assertEqual(response.status_code, 406)
+        self.assertEqual(json.loads(response.data), '[Invalid parameter input] value2 could not be 0.')
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
