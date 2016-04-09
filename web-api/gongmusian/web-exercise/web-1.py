0d619ac2f4f67c9699a954c1feff18ac949751fd
gongmusian <xp1245@hotmail.com>
2016-04-09 17:09:19 +0000

    set check_parameter(param, op)

--- web-api/gongmusian/web-exercise/web-1.py
+++ web-api/gongmusian/web-exercise/web-1.py
@@ -7,17 +7,24 @@ def isfloat(s):
     except ValueError:
         return False
 
-def check_parameter(param):
+def check_parameter(param, op):
+    msg = []
+    if op not in ['sum', 'minus', 'multiply', 'divide']:
+        msg.append('Operation is not availabe')
     if None in param:
-        return False, 406, 'Lose Key'
-    elif '' in param:
-        return False, 406, 'Lose Value'
+        msg.append('Lose key')
+    if '' in param:
+        msg.append('Lose Value')
+    for x in param:
+        # float() need string or number not None
+        if x != None and x != '':
+            if isfloat(x) == False:
+                msg.append('Value Type Error')
+    if len(msg) > 0:
+        # list to str
+        return False, 406, str(msg)[1:-1]
     else:
-        for x in param:
-            if isfloat(x) == False: 
-                return False, 406, 'Value Type Error'
-                break
-    return True, 200, 'OK'
+        return True, 200, 'OK'
 
 def float_2_dec(number):
     return ('%.2f' %number)
@@ -36,84 +43,21 @@ def count():
     param = []
     param.append(value1)
     param.append(value2)
-    chk, status, error_msg = check_parameter(param)
+    chk, status, error_msg = check_parameter(param, op)
     if chk:
         if op == 'sum':
-            return render_template('count.html', status=status, value1=value1, op='+', value2=value2, answer=float_2_dec(float(value1) + float(value2)), error_msg=error_msg)
+            return render_template('count.html', status=status, value1=value1, op='+', value2=value2, answer=float_2_dec(float(value1) + float(value2)), msg=error_msg)
         elif op == 'minus':
-            return render_template('count.html', status=status, value1=value1, op='-', value2=value2, answer=float_2_dec(float(value1) - float(value2)), error_msg=error_msg)
+            return render_template('count.html', status=status, value1=value1, op='-', value2=value2, answer=float_2_dec(float(value1) - float(value2)), msg=error_msg)
         elif op == 'multiply':
-            return render_template('count.html', status=status, value1=value1, op='X', value2=value2, answer=float_2_dec(float(value1) * float(value2)), error_msg=error_msg)
+            return render_template('count.html', status=status, value1=value1, op='×', value2=value2, answer=float_2_dec(float(value1) * float(value2)), msg=error_msg)
         elif op == 'divide':
             if float(value2) == 0:
-                return render_template('count.html', status=status, value1=value1, op='/', value2=value2, answer=" value2 can not equal 0", error_msg=error_msg)
+                return render_template('count.html', status=status, value1=value1, op='÷', value2=value2, answer=" value2 can not equal 0", error_msg=error_msg)
             else:
-                return render_template('count.html', status=status, value1=value1, op='/', value2=value2, answer=float_2_dec(float(value1) / float(value2)), error_msg=error_msg)
+                return render_template('count.html', status=status, value1=value1, op='÷', value2=value2, answer=float_2_dec(float(value1) / float(value2)), error_msg=error_msg)
     else:
        return render_template('count.html', msg=error_msg)
-
-@app.route('/sum', methods=['GET'])
-def sum():
-    sum_result = 0
-    value1 = request.args.get('value1')
-    value2 = request.args.get('value2')
-    param = []
-    param.append(value1)
-    param.append(value2)
-    chk, status, error_msg = check_parameter(param)
-    if chk:
-        sum_result = float_2_dec(float(value1) + float(value2))
-        return jsonify(sum=sum_result, status=200)
-    else:
-        return jsonify(Error= error_msg, status=status)
-
-@app.route('/minus', methods=['GET'])
-def minus():
-    minus_result = 0
-    value1 = request.args.get('value1')
-    value2 = request.args.get('value2')
-    param = []
-    param.append(value1)
-    param.append(value2)
-    chk, status, error_msg = check_parameter(param)
-    if chk:
-        minus_result = float_2_dec(float(value1) - float(value2))
-        return jsonify(minus=minus_result, status=200)
-    else:
-        return jsonify(Error=error_msg, status=status)
-
-@app.route('/multiply', methods=['GET'])
-def multiply():
-    mul_result = 0
-    value1 = request.args.get('value1')
-    value2 = request.args.get('value2')
-    param = []
-    param.append(value1)
-    param.append(value2)
-    chk, status, error_msg = check_parameter(param)
-    if chk:
-        mul_result = float_2_dec(float(value1) * float(value2))
-        return jsonify(multiply=mul_result, status=200)
-    else:
-        return jsonify(Error=error_msg, status=status)
-
-@app.route('/divide', methods=['GET'])
-def divide():
-    div_result = 0
-    value1 = request.args.get('value1')
-    value2 = request.args.get('value2')
-    param = []
-    param.append(value1)
-    param.append(value2)
-    chk, status, error_msg = check_parameter(param)
-    if chk:
-        if float(value2) == 0:
-            return jsonify(Error='Value2 shold not be zero!', status=status)
-        else:
-            div_result = float_2_dec(float(value1) / float(value2))
-            return jsonify(divide=div_result, status=200)
-    else:
-        return jsonify(Error=error_msg, status=status)
 
 @app.errorhandler(404)
 def page_no_found(e):
